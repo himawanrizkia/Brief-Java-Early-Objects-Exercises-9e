e45f0679e943a213fb700f80c1d4e70edefd57b0
himawanrizkia <kotaksurat.himawan@gmail.com>
2024-07-27 08:21:10 +0700

    Add files via upload

---
+++ E1_1.java
@@ -0,0 +1,17 @@
+/*
+PRACTICE EXERCISES E1.1
+Write a program that prints a greeting of your choice, perhaps in a language other than English.
+---
+Kita akan membuat program di Java yang dapat menampilkan salam yang sudah kita tentukan.
+*/
+
+// Deklarasikan nama class-nya dulu sebagai E1_1 (Nomor Soal). File ini sebaiknya nanti dinamai sebagai E1_1.java
+public class E1_1
+{
+// Deklarasikan metodenya, sesuai penjelasan di buku, deklarasikan metode main.
+    public static void main(String[] args)
+    {
+        //Tulis instruksi yang diperlukan dalam metode main, jangan lupa saat menuliskan instruksi berupa statement, akhiri dengan titik koma
+        System.out.println("Selamat pagi dunia!");
+    }
+}
